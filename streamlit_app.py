115d0b75f2af054c9cbaeee5e0ba29a8c1b6deda
Ramkumar <83807614+Ramkumar-commits@users.noreply.github.com>
2024-04-26 21:23:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
-import Fruityvice Dataframe as df
+import Fruityvice Dataframe as fv_df
 from snowflake.snowpark.functions import col
 
 # Write directly to the app

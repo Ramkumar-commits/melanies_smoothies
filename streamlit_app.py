df6d061c96d373b67cdd280da5659025362d9609
Ramkumar <83807614+Ramkumar-commits@users.noreply.github.com>
2024-04-26 21:04:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,8 +39,6 @@ if ingredients_list:
 	st.sub_header(fruit_chosen + 'Nutrition Information')
 	
 	    
-search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
 st.subheader(fruit_chosen + 'Nutrition Information')
 		

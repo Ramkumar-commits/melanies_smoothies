37e932125214a361b22299d55423c2788e969abd
Ramkumar <83807614+Ramkumar-commits@users.noreply.github.com>
2024-04-26 17:51:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,9 +44,8 @@ if ingredients_list:
     time_to_insert = st.button('Submit Order')
 
     if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-	    
-        st.success('Your Smoothie is ordered, {}!'.format(name_on_order),icon="✅" )
+    	session.sql(my_insert_stmt).collect()
+    	st.success('Your Smoothie is ordered, {}!'.format(name_on_order),icon="✅" )
 
 
 

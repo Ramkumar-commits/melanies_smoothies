d226aada906ae82db46849ba2994752bf0fe7f11
Ramkumar <83807614+Ramkumar-commits@users.noreply.github.com>
2024-04-26 17:31:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
-import requests
+from snowflake.snowpark.context import get_active_session
+
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -12,17 +13,10 @@ st.write(
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:',name_on_order)
 
-cnx = st.connection("snowflake")
-session = cnx.session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME')), col('SEARCH_ON')
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
-
-#Convert the Snowpark Dataframe to a Pandas Dataframe so we can use the LOC function
-pd_df=my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
 
 ingredients_list = st.multiselect(
       'Choose up to 5 ingredients:'
@@ -31,19 +25,13 @@ ingredients_list = st.multiselect(
 )
 
 if ingredients_list:
+    
+
     ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
 
-	    
-	search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-	st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
-	st.subheader(fruit_chosen + 'Nutrition Information')
-	fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
-	fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
-	    
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
@@ -59,7 +47,3 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 	    
         st.success('Your Smoothie is ordered, {}!'.format(name_on_order),icon="✅" )
-
-
-
-

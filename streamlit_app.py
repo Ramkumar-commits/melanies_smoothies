369360316c3672d32cecdfe559386df84a9e9296
Ramkumar <83807614+Ramkumar-commits@users.noreply.github.com>
2024-04-29 17:14:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,7 +33,7 @@ if ingredients_list:
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon" + fruit_chosen)
 fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
-	st.subheader(fruit_chosen + 'Nutrition Information')
+st.subheader(fruit_chosen + 'Nutrition Information')
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)

68ace6d6be65be5999f0eea2ec4be21bc593c679
Ramkumar <83807614+Ramkumar-commits@users.noreply.github.com>
2024-04-30 12:36:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-	    st.subheader(fruit_chosen + 'Nutrition Information')
+st.subheader(fruit_chosen + 'Nutrition Information')
 
 
 
